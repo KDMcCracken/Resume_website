cd900ed552219e627bd583ea38eda67a3474e5c0
Kenan <kdmccracken@hotmail.com>
2019-10-30 08:31:26 -0500

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-let today = new Date()
-let formatDate = today.toDateString()
-let selectElement = document.getElementById('date')
-selectElement.innerHTML = formatDate
+TweenMax.staggerFrom(".logo", 0.5, {opacity:0, rotation: 360, scale: 2, y:200, delay: 0.5}, 0.2)
+let leon, canvas, ctx;
 
-TweenMax.to(".logo", 2, {left:1600});
+const sw = 800;
+const sh = 600;
+const pixelRatio = 2;
+
+function init() {
+    canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+
+    canvas.width = sw * pixelRatio;
+    canvas.height = sh * pixelRatio;
+    canvas.style.width = sw + 'px';
+    canvas.style.height = sh + 'px';
+    ctx.scale(pixelRatio, pixelRatio);
+
+    leon = new LeonSans({
+        text: 'The quick brown\nfox jumps over\nthe lazy dog',
+        color: ['#000000'],
+        size: 80,
+        weight: 800
+    });
+
+    requestAnimationFrame(animate);
+}
+
+function animate(t) {
+    requestAnimationFrame(animate);
+
+    ctx.clearRect(0, 0, sw, sh);
+
+    const x = (sw - leon.rect.w) / 2;
+    const y = (sh - leon.rect.h) / 2;
+    leon.position(x, y);
+
+    leon.draw(ctx);
+}
+
+window.onload = () => {
+    init();
+};
+
+let i, total = leon.drawing.length;
+for (i = 0; i < total; i++) {
+    TweenMax.fromTo(leon.drawing[i], 1.6, {
+        value: 0
+    }, {
+        delay: i * 0.05,
+        value: 1,
+        ease: Power4.easeOut
+    });
+}
